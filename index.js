b8ab8245fe25b9b0a5e401cceec34a7577bdcf67
ashwin1321 <ashwinkhatiwada7@gmail.com>
2023-05-17 14:18:56 +0545

    initialize

---
+++ index.js
@@ -0,0 +1,13 @@
+const fastify = require('fastify')({ logger: true })       // logger is used to log the request and response
+
+
+// start the server
+const start = async () => {
+    try {
+        await fastify.listen({ port: 3000 })
+    } catch (err) {
+        fastify.log.error(err)
+        process.exit(1)
+    }
+}
+start()
