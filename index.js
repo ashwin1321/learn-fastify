0092de6f086b69a23539061b4364bbc3db2b7213
ashwin1321 <ashwinkhatiwada7@gmail.com>
2023-05-17 16:11:34 +0545

    feat: using fastify-swagger for the documentation

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const fastify = require('fastify')({ logger: true })       // logger is used to log the request and response
 const items = require('./items')                           // import the items array
 
+fastify.register(require('@fastify/swagger'), {
+    exposeRoute: true,
+    routePrefix: '/docs',
+    swagger: {
+        info: { title: 'fastify-api' },
+    },
+})
 fastify.register(require('./routes/itemsRoute'))                // register the items route
 // routes are defined in routes/itemsRoute.js file so we need to import it here so that we can register it with fastify and fastify will know about the routes that we have defined in the itemsRoute.js file and will be able to serve them.
 
