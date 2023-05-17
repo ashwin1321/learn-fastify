2406e65b5ffc687f751f94b52085f4c8f16e7e17
ashwin1321 <ashwinkhatiwada7@gmail.com>
2023-05-17 15:48:03 +0545

    feat: defining the handler function with the schema together

--- routes/itemsRoute.js
+++ routes/itemsRoute.js
@@ -18,6 +18,10 @@ const getItemsOpts = {
                 items: Item
             }
         }
+    },
+    // we can also define the handler function or controller in the getItemsOpts object, but we will define it separately below.
+    handler: async (request, reply) => {
+        reply.send(items)
     }
 }
 //the getItemsOpts object is defining the expected response structure for the /items endpoint, where the response should be an array of objects, with each object having an id and a name property, both of which should be strings.
@@ -28,6 +32,11 @@ const getItemOpts = {
         response: {
             200: Item
         }
+    },
+    handler: async (request, reply) => {
+        const { id } = request.params
+        const item = items.find(item => item.id === id)
+        reply.send(item)
     }
 }
 
@@ -37,20 +46,13 @@ function itemRoutes(fastify, options, done) {
     // we define a function that takes in fastify, options, and done as parameters. where fastify is the fastify instance, options is the options object passed to fastify.register(), and done is a callback that we need to call when we are done with the plugin registration.
 
     // get all items 
-    fastify.get('/items', getItemsOpts, async (request, reply) => {
-        // the gerItemsOpts is the second argument in the get() method. it is an object that defines the expected response structure for the /items endpoint, where the response should be an array of objects, with each object having an id and a name property, both of which should be strings.
-
-        reply.send(items)
-    })
+    fastify.get('/items', getItemsOpts)
+    // the gerItemsOpts is the second argument in the get() method. it is an object that defines the expected response structure for the /items endpoint, where the response should be an array of objects, with each object having an id and a name property, both of which should be strings.
 
     // get single item 
-    fastify.get('/items/:id', getItemOpts, async (request, reply) => {
-        const { id } = request.params
-        const item = items.find(item => item.id === id)
-        reply.send(item)
-    })
+    fastify.get('/items/:id', getItemOpts)
 
-    done()
+    done()  // we call the done() callback to signal that we are done with the plugin registration.
 }
 
 module.exports = itemRoutes
